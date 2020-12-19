14f748163addded3b97080eeddeb0c32f54d8de5
halid <halidozkilic@dkpeak.com>
2020-12-19 20:15:23 +0300

    update

---
+++ server/app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors')
+const logger = require('morgan')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+var indexRouter = require('./routes/index.js');
+
+//const config = require("./config/key");
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const mongoose = require("mongoose");
+
+
+mongoose.connect('mongodb://localhost/easy-duty', { useNewUrlParser: true ,useUnifiedTopology: true , useCreateIndex:true} );
+
+//ATLAS CONNECTION
+/* const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err)); */
+
+app.use(cors())
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(logger('dev'))
+
+
+
+app.use('/',indexRouter);
+app.use('/api/users', require('./routes/users'));
+
+
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+module.exports = app
